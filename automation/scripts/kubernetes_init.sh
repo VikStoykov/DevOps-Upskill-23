04406477e644d95d15e82b4efe52123a628167a6
vstoykov <viktor.stoykov.u23@learn.telerikacademy.com>
2024-01-21 11:31:04 +0200

    Fixes in Kubernetes init script
    Use Calico CNI

--- automation/scripts/kubernetes_init.sh
+++ automation/scripts/kubernetes_init.sh
@@ -2,6 +2,12 @@
 set -e
 set -x
 
+RUNC_VERSION="1.1.5"
+CONTAINERD_VERSION="1.6.2"
+KUBERNETES_VERSION="1.26.3"
+VIRTCTL_VERSION="v0.41.0"
+CALICO_VERSION="3.25.0"
+
 function install_necessery_components () {
     # Load overlay and br_netfilter 
     cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf 
@@ -29,8 +35,8 @@ EOF
     # sudo ufw disable
 
     # Download and install containerd
-    wget https://github.com/containerd/containerd/releases/download/v1.6.2/containerd-1.6.2-linux-amd64.tar.gz
-    sudo tar Cxzvf /usr/local containerd-1.6.2-linux-amd64.tar.gz
+    wget https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VERSION}/containerd-${CONTAINERD_VERSION}-linux-amd64.tar.gz
+    sudo tar Cxzvf /usr/local containerd-${CONTAINERD_VERSION}-linux-amd64.tar.gz
     wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
 
     sudo mkdir /etc/containerd
@@ -42,7 +48,7 @@ EOF
     sudo systemctl enable --now containerd
 
     # Download and install runc
-    wget https://github.com/opencontainers/runc/releases/download/v1.1.5/runc.amd64
+    wget https://github.com/opencontainers/runc/releases/download/v${RUNC_VERSION}/runc.amd64
     sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 
     # Download and install Kubernetes
@@ -50,7 +56,7 @@ EOF
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B53DC80D13EDEF05
     echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
     sudo apt update -y
-    sudo apt -y install vim git curl wget kubelet=1.26.3-00 kubeadm=1.26.3-00 kubectl=1.26.3-00
+    sudo apt -y install vim git curl wget kubelet=${KUBERNETES_VERSION}-00 kubeadm=${KUBERNETES_VERSION}-00 kubectl=${KUBERNETES_VERSION}-00
 
     # Configure crictl
     cat <<EOF | sudo tee /etc/crictl.yaml
@@ -62,24 +68,30 @@ EOF
 }
 
 function preprare_kubernetes () {
-    sudo kubeadm config images pull --cri-socket unix:///run/containerd/containerd.sock --kubernetes-version v1.26.3
+    sudo kubeadm config images pull --cri-socket unix:///run/containerd/containerd.sock --kubernetes-version v${KUBERNETES_VERSION}
+
+    wget https://github.com/kubevirt/kubevirt/releases/download/${VIRTCTL_VERSION}/virtctl-${VIRTCTL_VERSION}-linux-amd64
 }
 
 function start_cluster () {
     # Init cluster
-    sudo kubeadm init --upload-certs --kubernetes-version=v1.26.1  --ignore-preflight-errors=all  --cri-socket unix:///run/containerd/containerd.sock
+    sudo kubeadm init --upload-certs --kubernetes-version=v${KUBERNETES_VERSION}  --ignore-preflight-errors=all  --cri-socket unix:///run/containerd/containerd.sock
 
     sudo mkdir -p $HOME/.kube
     sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
-    export KUBECONFIG=/etc/kubernetes/admin.conf
+    #export KUBECONFIG=/etc/kubernetes/admin.conf
+
+    # Check for system is up and running
+    # kubectl get --raw='/readyz?verbose'
 
     # Run cluster in single mode
     sudo kubectl taint nodes --all node-role.kubernetes.io/control-plane:NoSchedule-
     sleep 10
 
-    # Apply Flannel CNI
-    sudo kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
+    # Apply Calico CNI
+    sudo kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/calico.yaml
+
 }
 
 install_necessery_components
