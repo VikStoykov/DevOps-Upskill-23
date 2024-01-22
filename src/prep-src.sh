fa640d0e45bcdd48cb88ba3118dac2a4ba1430fd
vstoykov <viktor.stoykov.u23@learn.telerikacademy.com>
2024-01-22 11:21:22 +0200

    Fix

--- src/prep-src.sh
+++ src/prep-src.sh
@@ -7,6 +7,6 @@ cd $KUBEVIRT_SRC
 
 for fname in ../src/patches/*.patch; do
   echo "Patch - $fname"
-  patch -p1 < 001-build.patch
+  patch -p1 < ../src/patches/001-build.patch
   #patch -p1 < $fname
 done
